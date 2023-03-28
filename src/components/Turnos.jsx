f65d56dc9c09f2684c306c2d6e5360fa36bad86b
AntonioDelGreco <antoniodelgreco@icloud.com>
2023-03-28 15:07:31 -0300

    Correciones

--- src/components/Turnos.jsx
+++ src/components/Turnos.jsx
@@ -28,17 +28,15 @@ const Turnos = () => {
   const [ formValues, setFormValues ] = useState(defaultFormTurno);
   const { nombre, celular, obraSocial, fecha, horario } = formValues;
   const alertaRef = useRef();
+  const horarioRef = useRef();
   const [ alerta, setAlerta ] = useState(defaultAlert); 
-  useEffect(() => {
-    alertaRef.current?.scrollIntoView({ behavior:'smooth' });
-  }, [alerta])
   
   //LOGICA
   const filterDate = date => date.getDay() === 2 || date.getDay() === 4;
   const resetValuesForm = () => {
     setFormValues(defaultFormTurno);    
   }
-
+  
   //RELLENANDO EL FORMULARIO
   const handleInputTurnos = event => {
     if(event instanceof Date){
@@ -49,7 +47,7 @@ const Turnos = () => {
       } else {
         hora = '18:00';
       }
-
+      
       setFormValues({
         ...formValues,
         fecha:valor,
@@ -61,7 +59,7 @@ const Turnos = () => {
         ...formValues,
         [name]:value
       })
-
+      
     }
   }
 
@@ -86,14 +84,21 @@ const Turnos = () => {
     resetValuesForm();
     setTimeout(() => setAlerta(defaultAlert), 3000);
   }
+  
+  useEffect(() => {
+    alertaRef.current?.scrollIntoView({ behavior:'smooth' });
+  }, [alerta])
+  useEffect(() => {
+    horarioRef.current?.scrollIntoView({ behavior:'smooth' });
+  }, [horario])
 
   return (
     <main className="contenedor py-10 overflow-x-hidden">
-      <form className='flex flex-col lg:flex-row items-center gap-20' onSubmit={handleFormSubmit}>
-        <div className='lg:w-1/3'>
-          <h2 className='border border-turqueza border-dos-esquinas p-5 text-center font-bold'>Importante: la reserva de turnos online es solo para primera consulta, por tratamientos mandar un mensaje de WhatsApp.</h2>
+      <form className='flex flex-col md:flex-row items-center gap-20' onSubmit={handleFormSubmit}>
+        <div className='md:w-1/3'>
+          <h2 className='border border-turqueza border-dos-esquinas p-5 text-center font-bold'>Importante: la reserva de turnos online es solo para primera consulta, por tratamientos mandar un mensaje de WhatsApp. Si no dispone de obra social, dejela en blanco.</h2>
         </div>
-        <div className='lg:w-1/2 overflow-x-hidden'>
+        <div className='w-full md:w-1/2'>
           <InputForm
             tipoInput='input'
             label='Nombre y Apellido:'
@@ -120,23 +125,21 @@ const Turnos = () => {
             value={obraSocial}
             onChange={handleInputTurnos}
             />
-          <div className='flex flex-col items-center md:flex-row gap-4 mb-16'>
-            <h2 className={`text-center text-grisClaro ${fecha && 'text-negro'}`}>Seleccionar fecha:</h2>
-            <DatePicker
-              className="border-b bg-casiBlanco border-grisClaro w-screen"
-              withPortal
-              locale={es}
-              name="fecha"
-              onChange={handleInputTurnos}
-              filterDate={filterDate}
-              selected={fecha}
-              />
-          </div>
+          <DatePicker
+            dateFormat="dd/MM/yyyy"
+            calendarClassName='date-picker-custom mb-10'
+            inline
+            locale={es}
+            name="fecha"
+            onChange={handleInputTurnos}
+            filterDate={filterDate}
+            selected={fecha}
+          />
           <Collapse isOpened={true}>
-            { horario && <p className='mb-16'>Su horario sera: <span className='font-bold text-3xl'>{horario}hs</span>. Existen dos horarios, uno para cada dia. De lo contrario comunicarse por WhatsApp.</p> }
+            { horario && <p ref={horarioRef} className='mb-16'>Su horario sera: <span className='font-bold text-3xl'>{horario}hs</span>. Existen dos horarios, uno para cada dia. De lo contrario comunicarse por WhatsApp.</p> }
           </Collapse>
         </div>
-        <div className='lg:w-1/3'>
+        <div className='md:w-1/3'>
           <Button tipo={'submit'} texto={'Solicitar turno'}/>
         </div>
       </form>
--- src/components/contacto/InputForm.jsx
+++ src/components/contacto/InputForm.jsx
@@ -2,7 +2,7 @@ const InputForm = ({tipoInput, label, ...otherProps}) => {
   return (
     <div className="group">
       {tipoInput === 'input' ? 
-      <input className='form-input ' {...otherProps}/>
+      <input className='form-input' {...otherProps}/>
       :
       <textarea className="form-input" {...otherProps} ></textarea>}
       <label className={`${otherProps.value.length > 0 ? 'shrink' : '' } form-input-label`}>{label}</label>
